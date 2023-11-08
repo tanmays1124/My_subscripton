7052cf3dbd137e6d7c7336038ec00d56dc6a2e87
tanmays1124 <57654142+tanmays1124@users.noreply.github.com>
2023-11-08 16:45:59 +0530

    Update subscription.py

--- subscription.py
+++ subscription.py
@@ -1,30 +1,34 @@
-n=int(input("Enter number of papers: "))
-bud=float(input("Enter the budget: "))
-cost=[]
-papers=[]
-while n>0:
-    k=1
-    sum1=0
-    paper=input("Enter the name of paper: ")
-    papers.append(paper)
-    while k<7:
-        a=float(input(f"Enter cost for day {k+1}: "))
-        sum1+=a
-        k+=1
-    cost.append(sum1)
-    n-=1
+# n=int(input("Enter number of papers: "))
+# bud=float(input("Enter the budget: "))
+# cost=[]
+# papers=[]
+# while n>0:
+#     k=1
+#     sum1=0
+#     paper=input("Enter the name of paper: ")
+#     papers.append(paper)
+#     while k<7:
+#         a=float(input(f"Enter cost for day {k+1}: "))
+#         sum1+=a
+#         k+=1
+#     cost.append(sum1)
+#     n-=1
 
 
-i=0
+# i=0
 
 
-l=[]
-for i in range(n):
-    j=i+1
-    while j<n:
-        if cost[i]+cost[j]<=bud:
-            a=[papers[i],papers[j]]
-            l.append(a)
-        j+=1
+# l=[]
+# for i in range(n):
+#     j=i+1
+#     while j<n:
+#         if cost[i]+cost[j]<=bud:
+#             a=[papers[i],papers[j]]
+#             l.append(a)
+#         j+=1
             
-print(l)
+# print(l)
+
+
+
+print("Subscription")
