01ea33391c57818b018c42868f2805d4216f036a
tanmays1124 <57654142+tanmays1124@users.noreply.github.com>
2023-11-10 14:03:49 +0530

    Update subscription.py

--- subscription.py
+++ subscription.py
@@ -1,4 +1,4 @@
-n=int(input("Enter number of papers: "))
+# n=int(input("Enter number of papers: "))
 # bud=float(input("Enter the budget: "))
 # cost=[]
 # papers=[]
@@ -32,4 +32,4 @@ n=int(input("Enter number of papers: "))
 # print(l)
 
 
-print("Subscription")
+print("Subscription file")
