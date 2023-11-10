0c459489d7eccc37a16a9d624b2ac5cdfec49891
tanmays1124 <tanmays1124@gmail.com>
2023-11-10 10:00:28 +0530

    added f3

--- ass.java
+++ ass.java
@@ -10,9 +10,9 @@ class ass {
 		// f is to ensures that numbers are float DATA TYPE
 		float f1 = 1.5f;
 		float f2 = 2.0f;
-
+float f3 = 32.4f;
 		// to store the multiplied value
-		float p = f1 * f2;
+		float p = f1 * f2 * f3;
 
 		// to print the product
 		System.out.println("The product is: " + p);
